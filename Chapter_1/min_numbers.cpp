8ac8b0bce3b809d3a0f1e919b38e720c89867c4f
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-02-12 17:53:33 +0000

    Updating the min_number sample code.

--- Chapter_1/min_numbers.cpp
+++ Chapter_1/min_numbers.cpp
@@ -6,8 +6,5 @@ int main()
     int min_of_int = std::min(100, 200);
     std::cout << "Min of 100 and 200 is " << min_of_int << std::endl;
 
-    double min_of_double = std::min(4.5, 2.3);
-    std::cout << "Min of 4.5 and 2.3 is " << min_of_double << std::endl;
-
-    return 0;    
+    return 0;
 }
