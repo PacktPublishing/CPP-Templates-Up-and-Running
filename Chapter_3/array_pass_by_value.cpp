d75341e4bcde59b3c24bdcde393aa882ec81db9b
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-03-03 13:21:30 +0000

    Updating example program for chapter 3.

--- Chapter_3/array_pass_by_value.cpp
+++ Chapter_3/array_pass_by_value.cpp
@@ -2,25 +2,16 @@
 template<typename T>
 void process_array(const T store, size_t len)
 {
-#if 0
     for(size_t index = 0; index < len; ++index) {
         store[index] = 100;
     }
     for(size_t index = 0; index < len; ++index)
         std::cout << store[index] << " ";
     std::cout << "\n";
-#endif 
-    *store = 100;
-    len = 20;
-    std::cout << *store << std::endl;
 }
-
 int main()
 {
-    //int mystore[] = {10, 20, 30, 40};
-    int x = 10;
-    int * ptr = &x;
-    process_array(ptr, 4);
- 
-   return 0;
+    int mystore[] = {10, 20, 30, 40};
+    process_array(mystore, 4);
+    return 0;
 }
