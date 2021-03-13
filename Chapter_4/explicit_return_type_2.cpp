dcd29b131a38784d0a17ab70a46028c3efb2f2d1
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-03-13 02:13:31 +0000

    Adding sample programs for Chapter 4.

---
+++ Chapter_4/explicit_return_type_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+template<typename T, typename U>
+U get_coef(T type)
+{
+    std::cout << type << std::endl;
+    U ret;
+    switch(type) {
+    case 1:
+        ret = 20.5;
+        break;
+    case 2:
+        ret = 100.5;
+        break;
+    default:
+        ret = 10.4;
+    }
+    return ret;
+}
+
+int main()
+{
+    int type = 2;
+    std::cout << get_coef<int, double>(type) << std::endl;
+    return 0;
+}
+
