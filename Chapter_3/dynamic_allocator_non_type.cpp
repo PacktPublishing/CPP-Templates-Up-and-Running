d272a7d23d845534b820ee85fa3bc0685f858299
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-06-09 18:17:22 +0100

    Modifying the source code for dynamic allocator with non type template parameter.

--- Chapter_3/dynamic_allocator_non_type.cpp
+++ Chapter_3/dynamic_allocator_non_type.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string.h>
-template<const size_t N, typename T>
+template<size_t N, typename T>
 T* dynamic_allocator(T val)
 {
     std::cout << "Value of N = " << N << std::endl;
@@ -17,17 +17,16 @@ int main()
         std::cout << iptr[index] << " ";
     std::cout << "\n";
 
-    const size_t len2 = 5;
-    auto dptr = dynamic_allocator<len2>(20.7);
-    for (size_t index = 0; index < len2; ++index)
+    auto dptr = dynamic_allocator<5>(20.7);
+    for (size_t index = 0; index < 5; ++index)
         std::cout << dptr[index] << " ";
     std::cout << "\n";
 
     const size_t len3 = 3;
     auto cptr = dynamic_allocator<len3>('C');
     for (size_t index = 0; index < len3; ++index)
-        std::cout << cptr[index] << std::endl;
-
+        std::cout << cptr[index] << " ";
+    std::cout << "\n";
     delete [] iptr;
     delete [] dptr;
     delete [] cptr;
