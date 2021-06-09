389d13f7e06a3dade76d6f28343227ddbf466ffa
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-06-09 12:00:03 +0100

    Adding sample code for non type parameter with enum.

---
+++ Chapter_3/non_type_enum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+enum myenum {
+    ONE = 1,
+    TWO = 2,
+    THREE =3
+};
+
+template <myenum val> //pointer to object
+void f()
+{
+   std::cout << val << std::endl;
+}
+
+int main()
+{
+    f<ONE>();
+    return 0;
+}
