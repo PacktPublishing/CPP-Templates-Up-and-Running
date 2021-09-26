5a06a146c4c519ddc922336ac7d35ca0f07f02f3
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-09-26 22:00:34 +0100

    Review comments modifications.

--- Chapter_6/custom_storage_class.cpp
+++ Chapter_6/custom_storage_class.cpp
@@ -15,13 +15,12 @@ public:
 
 MyStore::MyStore(size_t len) : len(len), curr_len(0)
 {
-    store = (int *) malloc(sizeof(int) * len);
+    store = new int[len];
 }
 
 MyStore::~MyStore()
 {
-    if(store)
-        free(store);
+    delete [] store;
 }
 
 void MyStore::add_element(int elem)
@@ -29,14 +28,7 @@ void MyStore::add_element(int elem)
     if(curr_len < len) {
         store[curr_len++] = elem;
     } else {
-        std::cout << "Number of elements added exceeds the allocated size" << std::endl;
-        std::cout << "Increasing the store size" << std::endl;
-        len = len + 100;
-        int* temp = (int *)realloc(store, sizeof(int) * len);
-        if (temp) {
-            store = temp;
-            store[curr_len++] = elem;
-        }
+        std::cout << "Maximum store size exceeded." << std::endl;
     }
 }
 
