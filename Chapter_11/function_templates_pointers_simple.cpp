bec3c62a3985e744ab92d1e3e7f8ffd50951e462
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-10-07 12:17:05 +0100

    Adding and modifying new source code for pointer type argument to template function.

--- Chapter_11/function_templates_pointers_simple.cpp
+++ Chapter_11/function_templates_pointers_simple.cpp
@@ -8,8 +8,21 @@ void set_value(T var)
 }
 int main()
 {
-    int myvar = 0;
-    set_value(&myvar);
-    std::cout << "Value at myvar = " << myvar << std::endl;
+    int * myptr = nullptr;
+    set_value(myptr);
+
+    if(myptr)
+     std::cout << "Value at myptr = " << *myptr << std::endl;
+    else
+     std::cout << "ERROR: myptr pointer is not initialized yet!" << std::endl;
+
+    myptr = new int;
+    set_value(myptr);
+    if(myptr)
+     std::cout << "Value at myptr = " << *myptr << std::endl;
+    else
+     std::cout << "ERROR: myptr pointer is not initialized yet!" << std::endl;
+
+    delete myptr;
     return 0;
 }
