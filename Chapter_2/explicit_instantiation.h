37b02273e7be92a3764f78ab5233d258e576d8c6
Vivek Bhadra <vivek.bhadra@gmail.com>
2021-10-19 11:29:36 +0100

    Adding copyright header for the source files.

--- Chapter_2/explicit_instantiation.h
+++ Chapter_2/explicit_instantiation.h
@@ -1,3 +1,13 @@
+/*************************************************************************\
+*                  Copyright (C) Vivek Bhadra, 2021.                      *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU General Public License as published by the   *
+* Free Software Foundation, either version 3 or (at your option) any      *
+* later version. This program is distributed without any warranty.  See   *
+* the file COPYING.gpl-v3 for details.                                    *
+\*************************************************************************/
+
 #ifndef __H_EXPLICIT_INST__
 #define __H_EXPLICIT_INST__
 #include <iostream>
@@ -7,5 +17,5 @@ T add(T x, T y)
     return x + y;
 }
 
-template int add<int>(int x, int y);
+//template int add<int>(int x, int y);
 #endif 
